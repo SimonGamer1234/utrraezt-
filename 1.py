93f6b7870f7f88a677702befeb5541414f341685
Simons <121897649+SimonGamer1234@users.noreply.github.com>
2025-01-24 23:48:47 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-import os
+rrimport os
 import random
 import requests
 import time
@@ -16,7 +16,7 @@ TOKEN1 = os.getenv("TOKEN_SCRT_1")
 TOKEN2 = os.getenv("TOKEN_SCRT_2")
 
 urls = URLS.split(',')
-
+Errors = []
 Ads = [AD1, AD2, AD3, AD4, AD5, AD6]
 Tokens = [TOKEN1, TOKEN2]
 tracker_file = "ad_tracker.txt"
@@ -43,7 +43,17 @@ for link in urls:
     try:
         res = requests.post(link, data=payload, headers=header)
         print(f"Posted to {link} : {res.status_code}")  # Print response status
+        if res.status_code != 200:
+            Errors.append((link,res.status_code,token_index))
     except requests.RequestException as e:
         print(f"Error posting to {link}: {e}")
     print(f"Waiting {sleeptime} seconds...")
     time.sleep(sleeptime)
+
+print(Errors)
+print(str(Errors))
+link1 = "https://discord.com/api/v9/channels/1300080115945836696/messages"
+header1 = {"Authorization": TOKEN1}
+payload1 = {"content": str(Errors)}
+res1 = requests.post(link1, data=payload1, headers=header1)
+print(f"Posted to {link1} : {res1.status_code}")  # Print response status
